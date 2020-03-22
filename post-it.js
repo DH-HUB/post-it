562e6d95122e59f6856537c9364b4a5f8434b8f3
DH-Hub <djerha@live.fr>
2020-03-22 11:50:43 +0100

    glisser post-it

--- post-it.js
+++ post-it.js
@@ -1,111 +1,77 @@
-//alert('Hello world'); test ok
+class becquet {
 
-/*class becquet{    hauteur; largeur;  Couleur; texte; visible; nonVisible ; position
-         constructor(   hauteur, largeur, Couleur, texte, visible, nonVisible , position)
-     {    this.position= position;     
-          this.nonVisible=nonVisible;         
-          this.visible=visible;
-          this.texte=texte;  
-          this.Couleur=Couleur;  
-          this.largeur=largeur;  
-          this.hauteur=hauteur;  }
+	constructor(x, y, vitesse, couleur) {
 
-          }*/
-          class becquet {
-            x;
-            y;
-            vitesse;
-            couleur;
-            postIt;f
-        
-            constructor(x, y, vitesse, couleur) {
-                this.x = x;
-                this.y = y;
-                this.vitesse = vitesse;
-                this.couleur = couleur;
-            }
-        
-            changeVitesse(vitesse) {
-                this.vitesse = vitesse;
-            }
-        
-            changePlace(x, y) {
-                this.x = x;
-                this.y = y;
-            }
-        
-            changeCouleur(coul) {
-                this.couleur = coul;
-            }
-            afficheTest() {
-                let monElem;
-                let creation = false;
-      
+		this.vitesse = vitesse;
+		
+		idMax = idMax + 1;
+		this.id = "postIt" + idMax; //Un id unique pour chaque postIt créé 
+		
+		let monElem = document.createElement('div');
+		monElem.id = this.id;
+		console.log("création " + monElem.id);
+		monElem.style.position = "fixed";
+		monElem.style.left = x + "px";
+		monElem.style.top = y + "px";
+		monElem.style.width = "150px";
+		monElem.style.height = "150px";
+		monElem.style.backgroundColor = couleur;
+		monElem.style.padding = "5px";
+		monElem.style.color = "black";
+		monElem.innerHTML = "vitesse : " + this.vitesse;
 
-            if (document.getElementById('posIt') == null){
-                //création post-it
-                monElem = document.createElement('div');
-                creation = true;
-            }
-            else {
-                //je la récupère
-                console.log("Mon elem Existe")
-              monElem = document.createElement('div');
-            }
-              
-                monElem.style.position = "fixed";
-                monElem.id = "posIt"
-                monElem.style.top = this.y + "px";
-                monElem.style.left = this.x + "px";
-                monElem.style.width = "150px";
-                monElem.style.height = "150px";
-                monElem.style.backgroundColor = this.couleur;
-                monElem.style.padding = "5px";
-                monElem.style.color = "black";
-                monElem.innerHTML = " " + this.vitesse + "";
-                document.body.appendChild(monElem);
+		// Gestion du Drag & Drop lorsqu'on clique sur l'objet
+		monElem.onmousedown = function(event) {
 
-                monElem.addEventListener('click', () => {
-                    console.log("on bouge !")
-                    onBouge = true;
-                })
-        
-            
-                if (creation) {
-                    document.body.appendChild(monElem);
-                }
-            }
-        }
-        
-       
+			let positionDansLePostItX = monElem.getBoundingClientRect().left;
+			let positionDansLePostItY = monElem.getBoundingClientRect().top;
+			let positionSourisX = event.clientX;
+			let positionSourisY = event.clientY;
+			let deplacementX = positionSourisX - positionDansLePostItX;
+			let deplacementY = positionSourisY - positionDansLePostItY;
+			console.log(positionSourisX + " - " + positionSourisY);
 
-let onBouge = false;
-let x;
-let y;
+			monElem.style.position = 'absolute';
+			monElem.style.zIndex = 1000; // place l'élément au premier plan
 
-let  monTest = new becquet(100, 200, 30, 'yellow');
-monTest . afficheTest ( ) ;	
-let violet  =  new becquet( 200 , 300 , 30 , "purple" ) ;	
-violet. afficheTest ( ) ;
-let vert =  new becquet( 300 , 400 , 30, "green" ) ;
-	
-vert . afficheTest ( ) ;
-document.addEventListener('mousemove', e => {
-    x = e.clientX;
-    y = e.clientY;
-})
-document.addEventListener('mouseup', () => {
-    console.log("on stop !")
-    onBouge = false;
-})
+			moveAt(event.pageX, event.pageY); // déplace le post-it (voir la fonction ci-dessous)
 
-function refresh() {
-    if (onBouge) {
-        console.log("je déplace !")
-  monTest.changePlace(x, y);
-  monTest.afficheTest();
-    }
-    setTimeout(refresh, 300)
+			function moveAt(x, y) {
+				// introduire ici la notion de vitesse
+				monElem.style.left = x - deplacementX + 'px';
+				monElem.style.top = y - deplacementY + 'px';
+			}
+
+			function onMouseMove(event) {
+				moveAt(event.pageX, event.pageY);
+			}
+
+			// Ajoute un écouteur sur le document pour détecter le mouvement de la souris
+			document.addEventListener('mousemove', onMouseMove); // la fonction onMouseMove est définie juste avant
+
+			// Evenement lorsqu'on relâche le bouton de la souris
+			monElem.onmouseup = function() {
+				// Supprime l'écouteur de mouvement de souris
+				document.removeEventListener('mousemove', onMouseMove);
+				monElem.onmouseup = null;
+			};
+
+		};
+
+		// Désactivation du Drag & Drop intégré du navigateur (pour ne pas entrer en conflit avec notre propre gestion du Drag & Drop)
+		monElem.ondragstart = function() {
+		  return false;
+		};
+		
+		// Crée le postIt sur la page
+		document.body.append(monElem);
+	}
 }
 
-refresh();
+let idMax = 0;
+
+// Création de 3 postIt (le constructeur crée l'objet et l'affiche)
+let jaune = new becquet(100, 200, 10, 'yellow');
+let violet  =  new becquet(200 , 300 , 20 , "purple");	
+let vert =  new becquet(300 , 400 , 30, "green");
+//Affichage du texte en cours
